34967c89c1b812dd20d8fa869807b4476dbe1441
Samuel Banya <sbanya@fastmail.com>
2025-12-01 15:49:28 -0600

    Adding initial project outline, goals, and notes

---
+++ main.py
@@ -0,0 +1,15 @@
+def directoryWizard:
+    pass;
+
+def obtainFileInfo:
+    # Determine matching file names:
+    # Determine hashes of matching files
+    # Determine sizes of matching files
+    pass;
+
+def __main__():
+    directoryWizard();
+    obtainFileInfo();
+
+if __name__ == '__main__':
+    main();
