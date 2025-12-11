ab82a4c1da42d150b921dc072c30fe6579cf7c21
Samuel Banya <sbanya@fastmail.com>
2025-12-11 17:04:37 -0600

    Adding basic changes to logic workflow

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import platform
+
 def directoryWizard:
+    # Values include 'Windows', 'Darwin', and 'Linux'
+    os_name = platform.system()
+
+    print("I see you are using ${os_name} for your operating system");
+    console_decision = input("What specific console are you scanning ROM metadata for?\n\nChoices include: '1': NES, '2': SNES, '3': Gameboy");
+    directory_location = input("Within the current directory where you are running this script, what is the name of the folder you want to scan for ROM's?");
+
     pass;
 
 def obtainFileInfo:
